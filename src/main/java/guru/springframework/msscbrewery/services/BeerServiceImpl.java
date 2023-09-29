7e2b7bd932d0f36c530671bcf78a6d3df132349c
bstojanovic <bstojanovic@cox.net>
2023-09-29 10:08:27 -0700

    Add put method and RequestBody annotation to post and put

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -17,4 +17,19 @@ public class BeerServiceImpl implements BeerService {
                 .beerStyle("Pale Ale")
                 .build();
     }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        //todo impl - would need to implement method
+    }
+
+
 }
--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -2,12 +2,10 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.services.BeerService;
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -28,6 +26,28 @@ public class BeerController {
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
 
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping // POST - Create New Beer
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto){
+
+        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+
+        //Send location of newly created resource (id) back to caller
+        HttpHeaders headers = new HttpHeaders();
+        //todo add hostname to url
+        headers.add("Location","/api/v1/beer/" + savedDto.getId().toString());
+
+        return new ResponseEntity(headers,HttpStatus.CREATED);
+    }
+    @PutMapping({"/{beerId}"})
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+        //Pass beerId as parameter, do not allow client to change beerId in body
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+        //Returns status code 204, telling client that request is performed but no content returned back to client.
+
     }
 
 }
